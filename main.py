3d9dfffd55b151b36cde2f6d06555a89bae8d0bd
arsenabdigali <abdigal.2015@gmail.com>
2025-11-23 19:17:43 +0500

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import gitlab
+
+gl = gitlab.Gitlab("https://gitlab.com", private_token="glpat-rvpjWsjGbPEN4hAAUiOCQ286MQp1Oml4bHVwCw.01.120ko7p0a")
+
+projects = gl.projects.list(membership=True)
+
+mr = projects[0].mergerequests.list()
+
+diffs = mr[0].diffs.list()
+
+diff = mr[0].diffs.get(diffs[0].get_id())
+
+for d in diff.diffs:
+    print("update: ", d['new_path'], d['diff'])
